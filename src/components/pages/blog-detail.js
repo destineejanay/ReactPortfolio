4076eee1f5ca4a0440b05beabcbebbb6dc0a2c30
Destinee Muse <destinee.muse@yahoo.com>
2022-07-05 16:51:32 -0500

    updated blog-form, blog-feature-image, rich-text-editor, added text-editor styles, blog-detail updates added image uploader to modal, added blog edit function

--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import axios from "axios";
 import ReactHtmlParser from "react-html-parser";
+
+import BlogForm from "../blog/blog-form";
 import BlogFeaturedImage from "../blog/blog-featured-image";
 
 export default class BlogDetail extends Component {
@@ -10,7 +12,7 @@ export default class BlogDetail extends Component {
     this.state = {
       currentId: this.props.match.params.slug,
       blogItem: {},
-      editMode: false,
+      editMode: false
     };
 
     this.handleEditClick = this.handleEditClick.bind(this);
@@ -18,19 +20,22 @@ export default class BlogDetail extends Component {
 
   handleEditClick() {
     console.log("handle edit clicked");
+    this.setState({ editMode: true });
   }
 
   getBlogItem() {
     axios
       .get(
-        `https://destineemuse.devcamp.space/portfolio/portfolio_blogs/${this.state.currentId}`
+        `https://jordan.devcamp.space/portfolio/portfolio_blogs/${
+          this.state.currentId
+        }`
       )
-      .then((response) => {
+      .then(response => {
         this.setState({
-          blogItem: response.data.portfolio_blog,
+          blogItem: response.data.portfolio_blog
         });
       })
-      .catch((error) => {
+      .catch(error => {
         console.log("getBlogItem error", error);
       });
   }
@@ -40,19 +45,29 @@ export default class BlogDetail extends Component {
   }
 
   render() {
-    const { title, content, featured_image_url, blog_status } =
-      this.state.blogItem;
+    const {
+      title,
+      content,
+      featured_image_url,
+      blog_status
+    } = this.state.blogItem;
 
-    return (
-      <div className="blog-container">
-        <div className="content-container">
-          <h1 onClick={this.handleEditClick}>{title}</h1>
+    const contentManager = () => {
+      if (this.state.editMode) {
+        return <BlogForm editMode={this.state.editMode} blog={this.state.blogItem} />;
+      } else {
+        return (
+          <div className="content-container">
+            <h1 onClick={this.handleEditClick}>{title}</h1>
 
-          <BlogFeaturedImage img={featured_image_url} />
-          
-          <div className="content">{ReactHtmlParser(content)}</div>
-        </div>
-      </div>
-    );
+            <BlogFeaturedImage img={featured_image_url} />
+
+            <div className="content">{ReactHtmlParser(content)}</div>
+          </div>
+        );
+      }
+    };
+
+    return <div className="blog-container">{contentManager()}</div>;
   }
 }
